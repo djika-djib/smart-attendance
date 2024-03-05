729b4a3194acc8d32ab6392c623fda5f2a5c301b
dennis <asiedudennis30@gmail.com>
2024-03-05 12:52:03 +0000

    Started Creating the homepage

--- smart_attendance_system/settings.py
+++ smart_attendance_system/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'attendance'
+    'attendance.apps.AttendanceConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
